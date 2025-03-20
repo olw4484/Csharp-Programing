e579a24e5707eab6b21119337c16a5c9813a6953
owl4484 <rodeo23267286@gmail.com>
2025-03-20 12:57:52 +0900

    250320
    8일차 깃 테스트용

---
+++ 08.Day8_git_test/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08.Day8_git_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("git_Tset용 txt입니다.");
+            Console.WriteLine("git_Tset용 txt입니다.");
+            Console.WriteLine("git_Tset용 txt입니다.");
+            Console.WriteLine("git_Tset용 txt입니다.");
+
+        }
+    }
+}
